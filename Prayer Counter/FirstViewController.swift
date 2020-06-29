fea7ea4a7df5b0c4070305e73b8604498f14f46f
Muhammad Albayati <malbayat@uci.edu>
2020-06-28 22:13:30 -0700

    initial commit

---
+++ Prayer Counter/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Prayer Counter
+//
+//  Created by Muhammad Albayati on 6/28/20.
+//  Copyright © 2020 Muhammad Albayati. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Prayer Counter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Prayer Counter
+//
+//  Created by Muhammad Albayati on 6/28/20.
+//  Copyright © 2020 Muhammad Albayati. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
